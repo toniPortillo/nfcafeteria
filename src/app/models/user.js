650ac225dadfe9cedf13f68d6796f4430aeab35e
Toni <toniplsec@gmail.com>
2017-11-20 17:38:10 +0100

    Estructura y primera iteracion

---
+++ src/app/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+const userSchema = new mongoose.Schema({
+    local: {
+        email: String,
+        password: String
+    },
+    facebook: {
+        email: String,
+        password: String,
+        id: String,
+        token: String
+    },
+    twitter: {
+        email: String,
+        password: String,
+        id: String,
+        token: String
+    },
+    google: {
+        email: String,
+        password: String,
+        id: String,
+        token: String
+    },
+});
+
+userSchema.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
---
+++ src/config/passport.js
@@ -0,0 +1,58 @@
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../app/models/user');
+
+module.exports = function(passport) {
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+
+    // signup
+    passport.use('local-signup', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function (req, email, password, done) {
+        User.findOne({'local.email': email}, function (err, user) {
+            if (err) { return done(err); }
+            if (user) {
+                return done(null, false, req.flash('signupMessage', 'The email is already taken'));
+            } else {
+                var newUser = new User();
+                newUser.local.email = email;
+                newUser.local.password = newUser.generateHash(password);
+                newUser.save(function (err) {
+                    if (err) {throw err;}
+                    return done(null, newUser); 
+                });
+            }
+        })
+    }));
+
+    // login
+    passport.use('local-login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function (req, email, password, done) {
+        User.findOne({'local.email': email}, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'No User found'));
+            } 
+            if (!user.validatePassword(password)) {
+                return done(null, false, req.flash('loginMessage', 'Wrong password'));
+            }
+            return done(null, user);
+        })
+    }));
+}
